32a304fecbf4de50c8e24a69631b627e4d9f596f
DxnnisKx <denis.makkar14@gmail.com>
2025-05-19 14:15:30 +0200

    MYSQL Datenbank implementiert
    MYSQL Datenbank implementiert - funktioniert noch nicht für Acid und Skeletton

--- Assets/Script/DeathZoneManager.cs
+++ Assets/Script/DeathZoneManager.cs
@@ -1,12 +1,24 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using MySql.Data.MySqlClient;
+using System;
 
 public class UpdateDeathZonePosition : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private SceneAsset scene;
     // Update is called once per frame
+    private MeterDisplay meterDisplay; // Reference to MeterDisplay component
+
+    private void Start()
+    {
+        // Get reference to the MeterDisplay component
+        meterDisplay = FindFirstObjectByType<MeterDisplay>();
+
+        // Alternative approach: if MeterDisplay is on the player object
+        // meterDisplay = player.GetComponent<MeterDisplay>();
+    }
     void Update()
     {
         gameObject.transform.position = new Vector2(player.transform.position.x, gameObject.transform.position.y);
@@ -16,7 +28,41 @@ public class UpdateDeathZonePosition : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            int currentScore = 0;
+            if (meterDisplay != null)
+            {
+                currentScore = meterDisplay.meterInt;
+            }
+            else
+            {
+                Debug.LogError("MeterDisplay component not found!");
+            }
             SceneManager.LoadScene(scene.name);
+            string server = "192.168.231.159"; // IP deines Raspberry Pi
+            string database = "EK_HighScores";
+            string user = "pi";
+            string password = "raspberry";
+
+            string connectionString = $"Server={server};Database={database};User ID={user};Password={password};";
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "INSERT INTO scores (score) VALUES (@score)";
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@score", currentScore);  // Beispielwert
+                        cmd.ExecuteNonQuery();
+                        Console.WriteLine("Datensatz erfolgreich eingefügt!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fehler: " + ex.Message);
+            }
         }
          //Delete Enemies on touch
         if (collision.gameObject.CompareTag("Enemy"))
--- Assets/Script/Meter.cs
+++ Assets/Script/Meter.cs
@@ -4,19 +4,21 @@ using TMPro;  // TextMeshPro Namespace hinzufügen
 public class MeterDisplay : MonoBehaviour
 {
     public TMP_Text meterText;  // TextMeshPro statt normales Text-Element
+    public int meterInt;
     public Transform player;  // Der Spieler, dessen Position gemessen wird
-    private float startPosX;  // Startposition des Spielers auf der X-Achse
+    private int startPosX;  // Startposition des Spielers auf der X-Achse
 
     void Start()
     {
         // Speichert die Startposition des Spielers
-        startPosX = player.position.x;
+        startPosX = (int)player.position.x;
     }
 
     void Update()
     {
         // Berechnet die Meter relativ zur Startposition
-        float meters = player.position.x - startPosX;
+        int meters = (int)player.position.x - startPosX;
+        meterInt = meters;
 
         // Zeigt die Meterzahl an, mit 2 Dezimalstellen und verhindert negative Meterzahlen
         meterText.text = Mathf.Max(0, meters).ToString("N0") + " m";
